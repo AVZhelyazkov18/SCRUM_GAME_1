177ff08456839e6f22a30d6979d863495cd8e0cd
PrintHell0 <AckPrintHell0@users.noreply.github.com>
2020-11-07 10:19:27 +0200

    Update 0.0v
    - Main Menu is fully functional
    - Can play game
    (Game has no functionality yet)

--- Scrum_Game/Assets/Scripts/ButtonScripts.cs
+++ Scrum_Game/Assets/Scripts/ButtonScripts.cs
@@ -8,9 +8,12 @@ public class ButtonScripts : MonoBehaviour
     private Button optionsButton;
     private Button backButton;
     private Button exitButton;
+    private Button playButton;
+    private Button backButton2;
 
     private Canvas Menu;
     private Canvas Options;
+    private Canvas minigameMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -18,26 +21,41 @@ public class ButtonScripts : MonoBehaviour
         backButton = GameObject.Find("BackButton").GetComponent<Button>();
         optionsButton = GameObject.Find("OptionsButton").GetComponent<Button>();
         exitButton = GameObject.Find("ExitButton").GetComponent<Button>();
+        playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        backButton2 = GameObject.Find("BackButton2").GetComponent<Button>();
 
         backButton.onClick.AddListener(BackButtonClick);
         optionsButton.onClick.AddListener(OptionsClick);
         exitButton.onClick.AddListener(QuitGame);
+        playButton.onClick.AddListener(onPlayClick);
+        backButton2.onClick.AddListener(BackButtonClick);
 
         Menu = GameObject.Find("MenuUI").GetComponent<Canvas>();
         Options = GameObject.Find("OptionsUI").GetComponent<Canvas>();
+        minigameMenu = GameObject.Find("GameTypeUI").GetComponent<Canvas>();
 
+        minigameMenu.enabled = false;
         Menu.enabled = true;
+        Options.enabled = false;
+    }
+
+    private void onPlayClick()
+    {
+        minigameMenu.enabled = true;
+        Menu.enabled = false;
         Options.enabled = false;
     }
 
     private void BackButtonClick()
     {
+        minigameMenu.enabled = false;
         Menu.enabled = true;
         Options.enabled = false;
     }
 
     private void OptionsClick() // Function is called when the Options button is clicked
     {
+        minigameMenu.enabled = false;
         Menu.enabled = false;
         Options.enabled = true;
     }
---
+++ Scrum_Game/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LoadGame : MonoBehaviour
+{
+    private Button catchGame = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        catchGame = GameObject.Find("Choice1").GetComponent<Button>();
+        catchGame.onClick.AddListener(startCatchGame);
+    }
+
+    void startCatchGame()
+    {
+        Debug.Log("Starting the game!");
+        SceneManager.LoadScene("FishGame");
+    }
+}
